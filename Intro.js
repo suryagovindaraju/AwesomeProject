21f4d44cfd256f9ebf97942122c593878f0a6d09
Surya0708 <external.surya.adabala@inter-ikea.com>
2019-01-03 11:41:57 +0530

    minor change

--- Intro.js
+++ Intro.js
@@ -7,7 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
